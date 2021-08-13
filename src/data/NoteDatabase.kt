a4292ca0a4aed845cbfe55423c09b9678ed0375d
Ugis <ozols.ugiss@gmail.com>
2021-08-13 21:47:04 +0300

    folder & notes save and delete functions created

--- src/data/NoteDatabase.kt
+++ src/data/NoteDatabase.kt
@@ -38,3 +38,34 @@ suspend fun getFolderForUser(email: String) : List<Folder> {
 suspend fun getNoteForUser(email: String) : List<Note> {
     return notes.find(Note::userEmail eq email).toList()
 }
+
+suspend fun saveFolder(folder: Folder) : Boolean {
+    val folderExists = folders.findOneById(folder.id)!= null
+    return if (folderExists){
+                folders.updateOneById(folder.id, folder).wasAcknowledged()
+            }else{
+                folders.insertOne(folder).wasAcknowledged()
+    }
+}
+suspend fun saveNote(note: Note) : Boolean {
+    val noteExists = notes.findOneById(note.id)!= null
+    return if (noteExists){
+        notes.updateOneById(note.id, note).wasAcknowledged()
+    }else{
+        notes.insertOne(note).wasAcknowledged()
+    }
+}
+
+suspend fun deleteNote(email: String, noteId : String) : Boolean{
+    val note = notes.findOne(Note::id eq noteId, Note::userEmail eq email)
+    note?.let { note->
+        return notes.deleteOneById(note.id).wasAcknowledged()
+    } ?: return false
+}
+
+suspend fun deleteFolder(email: String, folderId : String) : Boolean {
+    val folder = folders.findOne(Folder::id eq folderId, Folder::userEmail eq email)
+    folder?.let { folder->
+        return folders.deleteOneById(folder.id).wasAcknowledged()
+    } ?: return false
+}
