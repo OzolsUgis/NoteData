6171989c521695171ecfa74e13cc0ad62091a145
Ugis <ozols.ugiss@gmail.com>
2021-08-13 19:21:26 +0300

    all properties installed TODO - Basic authentication configuration

--- src/Application.kt
+++ src/Application.kt
@@ -1,13 +1,30 @@
 package com.ugisozols
 
 import io.ktor.application.*
+import io.ktor.auth.*
+import io.ktor.features.*
+import io.ktor.gson.*
 import io.ktor.response.*
 import io.ktor.request.*
+import io.ktor.routing.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+    install(ContentNegotiation){
+        gson {
+            setPrettyPrinting()
+        }
+    }
+    install(CallLogging)
+    install(DefaultHeaders)
+    install(Authentication)
+    install(Routing){
+
+    }
+
+
 }
 
