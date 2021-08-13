b6d2c43b27aa2afb27e152ab03aa33fb2e9865c8
Ugis <ozols.ugiss@gmail.com>
2021-08-13 19:54:43 +0300

    Note collection specified

---
+++ src/data/collections/Note.kt
@@ -0,0 +1,12 @@
+package com.ugisozols.data.collections
+
+import org.bson.codecs.pojo.annotations.BsonId
+import org.bson.types.ObjectId
+
+data class Note(
+    val title : String,
+    val date : Long,
+    val content : String,
+    @BsonId
+    val id : String = ObjectId().toString()
+)
