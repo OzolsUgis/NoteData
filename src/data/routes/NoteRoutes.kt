84100ca211016cf0450f95a45607607da82bb554
Ugis <ozols.ugiss@gmail.com>
2021-08-14 15:43:16 +0300

    save note, get note & delete note functions created

---
+++ src/data/routes/NoteRoutes.kt
@@ -0,0 +1,63 @@
+package com.ugisozols.data.routes
+
+import com.ugisozols.data.*
+import com.ugisozols.data.collections.Folder
+import com.ugisozols.data.collections.Note
+import com.ugisozols.data.collections.User
+import com.ugisozols.data.requests.DeleteFolder
+import com.ugisozols.data.requests.DeleteNote
+import io.ktor.application.*
+import io.ktor.auth.*
+import io.ktor.http.*
+import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.routing.*
+
+fun Route.noteRoutes(){
+    route("/getNotes"){
+        authenticate {
+            get {
+                val email = call.principal<UserIdPrincipal>()!!.name
+                val notes = getNoteForUser(email)
+                call.respond(HttpStatusCode.OK, notes)
+            }
+        }
+    }
+
+    route("/addNotes"){
+        authenticate {
+            post {
+                val note = try {
+                    call.receive<Note>()
+                }catch (e: ContentTransformationException){
+                    call.respond(HttpStatusCode.BadRequest)
+                    return@post
+                }
+                if (saveNote(note)){
+                    call.respond(HttpStatusCode.OK)
+                }else{
+                    call.respond(HttpStatusCode.Conflict)
+                }
+            }
+        }
+    }
+
+    route("deleteNote"){
+        authenticate {
+            post {
+                val email = call.principal<UserIdPrincipal>()!!.name
+                val request = try {
+                    call.receive<DeleteNote>()
+                }catch (e:ContentTransformationException){
+                    call.respond(HttpStatusCode.BadRequest)
+                    return@post
+                }
+                if(deleteFolder(email,request.noteId)){
+                    call.respond(HttpStatusCode.OK)
+                }else{
+                    call.respond(HttpStatusCode.Conflict)
+                }
+            }
+        }
+    }
+}
